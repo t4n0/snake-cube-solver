61468c35dfbdab68e281cf23f77f51803d8216e7
Sebastian Carter <Sebastian.Carter@web.de>
2022-04-24 22:19:51 +0200

    Print basics at entry

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include "quaternion.h"
 
+#include <iostream>
+
 int main()
 {
+    std::cout << "Solving cube puzzle with brute force.\n";
+    std::cout << "Attempting to calculate up to 4.294.967.296 solutions.\n";  // = 4^16
+
     return 0;
 }
