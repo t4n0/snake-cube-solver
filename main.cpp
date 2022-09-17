76b52b4b935df7f8c4a2ab0c53a9cac1498758f2
Sebastian Carter <Sebastian.Carter@web.de>
2022-09-17 16:46:55 +0200

    Generate global vertices per block on the fly

--- main.cpp
+++ main.cpp
@@ -9,46 +9,87 @@
 #include <iostream>
 #include <thread>
 
+const math::Quaternion kOrigin{0.0, 0.0, 0.0};
 const math::Quaternion kNullOrientation{1.0, 0.0, 0.0, 0.0};
 const math::Quaternion kQuarterPitch{math::CreateRotation(math::TAU / 4.0, 0.0, 1.0, 0.0)};
 const math::Quaternion kQuarterRoll{math::CreateRotation(math::TAU / 4.0, 1.0, 0.0, 0.0)};
 
-struct Beam
+struct Block
 {
-    math::Quaternion orientation{kNullOrientation};  // Relative to parent system.
-    int blocks{0};
+    math::Quaternion local_orientation{kNullOrientation};  // Relative to previous block.
+    math::Quaternion global_orientation{kOrigin};
+    math::Quaternion global_location{kOrigin};
+    bool may_roll{false};
 };
 
-std::ostream& operator<<(std::ostream& ostream, const Beam& beam)
+std::ostream& operator<<(std::ostream& ostream, const Block& block)
 {
-    ostream << "orientation: " << beam.orientation << '\n';
-    ostream << "blocks: " << beam.blocks;
+    ostream << "local_orientation: " << block.local_orientation << '\n';
+    ostream << "global_orientation: " << block.global_orientation << '\n';
+    ostream << "global_location: " << block.global_location << '\n';
+    ostream << "may_roll: " << block.may_roll << '\n';
     return ostream;
 }
 
-using Cube = std::array<Beam, 17>;
+using Cube = std::array<Block, 27>;
 
 void LogCube(const Cube& cube)
 {
-    for (const auto& beam : cube)
+    for (const auto& block : cube)
     {
-        std::cout << beam.orientation << '\n';
+        std::cout << block << '\n';
     }
     std::cout << std::endl;
 }
 
 Cube CreateFlatCube()
 {
-    constexpr std::array<int, 17> kBeamLengths{3, 3, 3, 3, 2, 2, 2, 3, 3, 2, 2, 3, 2, 3, 2, 2, 3};
+    const math::Quaternion invalid{0.0, 0.0, 0.0};
+    const auto start_block = Block{kNullOrientation, kNullOrientation, kOrigin, false};
+    const auto passive_block = Block{kNullOrientation, invalid, invalid, false};
+    const auto quarter_pitched_pos = Block{kQuarterPitch, invalid, invalid, true};
+    const auto quarter_pitched_neg = Block{kQuarterPitch.Inverse(), invalid, invalid, true};
+    const auto passive_quarter_pitched_pos = Block{kQuarterPitch, invalid, invalid, false};
 
     Cube cube{};
-    cube.at(0) = Beam{kNullOrientation, kBeamLengths.front()};
 
-    auto next_orientation = math::Quaternion{kQuarterPitch};
-    for (std::size_t idx{1}; idx < kBeamLengths.size(); idx++)
+    // Fill local orientations. These won't change.
+    cube.at(0) = start_block;  // 1 (Rolling would rotate entire cube.)
+    cube.at(1) = passive_block;
+    cube.at(2) = passive_block;
+    cube.at(3) = quarter_pitched_neg;  // 2
+    cube.at(4) = passive_block;
+    cube.at(5) = quarter_pitched_pos;  // 3
+    cube.at(6) = passive_block;
+    cube.at(7) = quarter_pitched_neg;  // 4
+    cube.at(8) = passive_block;
+    cube.at(9) = quarter_pitched_pos;   // 5
+    cube.at(10) = quarter_pitched_neg;  // 6
+    cube.at(11) = quarter_pitched_pos;  // 7
+    cube.at(12) = quarter_pitched_neg;  // 8
+    cube.at(13) = passive_block;
+    cube.at(14) = quarter_pitched_pos;  // 9
+    cube.at(15) = passive_block;
+    cube.at(16) = quarter_pitched_neg;  // 10
+    cube.at(17) = quarter_pitched_pos;  // 11
+    cube.at(18) = quarter_pitched_neg;  // 12
+    cube.at(19) = passive_block;
+    cube.at(20) = quarter_pitched_pos;  // 13
+    cube.at(21) = quarter_pitched_neg;  // 14
+    cube.at(22) = passive_block;
+    cube.at(23) = quarter_pitched_pos;          // 15
+    cube.at(24) = quarter_pitched_neg;          // 16
+    cube.at(25) = passive_quarter_pitched_pos;  // 17 (Rolling would have no effect.)
+    cube.at(26) = passive_block;
+
+    // Calculate global poses.
+    const auto single_block = math::Quaternion{1.0, 0.0, 0.0};
+    for (std::size_t index{1}; index < cube.size(); index++)
     {
-        next_orientation = next_orientation.Inverse();
-        cube.at(idx) = Beam{next_orientation, kBeamLengths.at(idx)};
+        cube.at(index).global_orientation =
+            cube.at(index).local_orientation.AppendAsLocalRotationAfter(cube.at(index - 1UL).global_orientation);
+        cube.at(index).global_location =
+            cube.at(index - 1UL).global_location + single_block.RotateBy(cube.at(index).global_orientation);
     }
 
     return cube;
@@ -56,39 +97,15 @@ Cube CreateFlatCube()
 
 using Vertices = std::tuple<std::array<double, 27>, std::array<double, 27>, std::array<double, 27>>;
 
-void LogVertices(const Vertices& vertices)
+Vertices UnweaveCoordinates(const Cube& cube)
 {
-    const auto& [x, y, z] = vertices;
-    for (std::size_t index{0}; index < x.size(); index++)
+    Vertices x_y_z{};
+    for (std::size_t index{0}; index < cube.size(); index++)
     {
-        std::cout << x.at(index) << " " << y.at(index) << " " << z.at(index) << '\n';
-    }
-    std::cout << std::endl;
-}
-
-Vertices GenerateVertices(const Cube& cube)
-{
-    Vertices x_y_z;
-    std::get<0>(x_y_z).at(0) = 0.0;
-    std::get<1>(x_y_z).at(0) = 0.0;
-    std::get<2>(x_y_z).at(0) = 0.0;
-
-    math::Quaternion current_location{0.0, 0.0, 0.0};        // In global coordinates.
-    math::Quaternion current_orientation{kNullOrientation};  // In global coordinates.
-    std::size_t index{1};
-    for (const auto& beam : cube)
-    {
-        current_orientation = beam.orientation.AppendAsLocalRotationAfter(current_orientation);
-        const auto single_block = math::Quaternion{1.0, 0.0, 0.0}.RotateBy(current_orientation);
-        for (int blocks{1}; blocks < beam.blocks; blocks++)
-        {
-            current_location += single_block;
-            const auto current_coordinates = current_location.GetVectorPart();
-            std::get<0>(x_y_z).at(index) = current_coordinates.at(0);
-            std::get<1>(x_y_z).at(index) = current_coordinates.at(1);
-            std::get<2>(x_y_z).at(index) = current_coordinates.at(2);
-            index++;
-        }
+        const auto current_coordinates = cube.at(index).global_location.GetVectorPart();
+        std::get<0>(x_y_z).at(index) = current_coordinates.at(0);
+        std::get<1>(x_y_z).at(index) = current_coordinates.at(1);
+        std::get<2>(x_y_z).at(index) = current_coordinates.at(2);
     }
 
     return x_y_z;
@@ -96,39 +113,55 @@ Vertices GenerateVertices(const Cube& cube)
 
 void Plot(const Cube& cube)
 {
-    const auto vertices = GenerateVertices(cube);
+    const auto vertices = UnweaveCoordinates(cube);
     const auto& [x, y, z] = vertices;
 
-    static auto figure = matplot::figure<matplot::backend::opengl_embed>();
+    static auto figure = matplot::figure();
     figure->current_axes()->axis(matplot::equal);
     auto l = figure->current_axes()->plot3(x, y, z, "");
     l->line_width(2);
     LogCube(cube);
-    LogVertices(vertices);
     std::cin.ignore();
 }
 
 const int kTotalPossibleRotations{1073741824};  // = 4^15
 
-bool VerticesFitIn3by3Box(const Vertices& vertices)
+double GetX(const Block& block)
 {
-    const auto& [x, y, z] = vertices;
-    const auto x_minmax = std::minmax_element(x.begin(), x.end());
-    const auto x_diff = (*x_minmax.second) - (*x_minmax.first);
+    return std::get<0>(block.global_location.GetVectorPart());
+}
+
+double GetY(const Block& block)
+{
+    return std::get<1>(block.global_location.GetVectorPart());
+}
+
+double GetZ(const Block& block)
+{
+    return std::get<2>(block.global_location.GetVectorPart());
+}
+
+bool BlocksFitIn3by3Box(const Cube& cube)
+{
+    const auto x_minmax =
+        std::minmax_element(cube.begin(), cube.end(), [](const Block& a, const Block& b) { return GetX(a) < GetX(b); });
+    const auto x_diff = GetX(*x_minmax.second) - GetX(*x_minmax.second);
     if (x_diff > 2.01)
     {
         return false;
     }
 
-    const auto y_minmax = std::minmax_element(y.begin(), y.end());
-    const auto y_diff = (*y_minmax.second) - (*y_minmax.first);
+    const auto y_minmax =
+        std::minmax_element(cube.begin(), cube.end(), [](const Block& a, const Block& b) { return GetY(a) < GetY(b); });
+    const auto y_diff = GetY(*y_minmax.second) - GetY(*y_minmax.second);
     if (y_diff > 2.01)
     {
         return false;
     }
 
-    const auto z_minmax = std::minmax_element(z.begin(), z.end());
-    const auto z_diff = (*z_minmax.second) - (*z_minmax.first);
+    const auto z_minmax =
+        std::minmax_element(cube.begin(), cube.end(), [](const Block& a, const Block& b) { return GetZ(a) < GetZ(b); });
+    const auto z_diff = GetZ(*z_minmax.second) - GetZ(*z_minmax.second);
     if (z_diff > 2.01)
     {
         return false;
@@ -137,25 +170,36 @@ bool VerticesFitIn3by3Box(const Vertices& vertices)
     return true;
 }
 
-double SquaredDistance(const double x1,
-                       const double y1,
-                       const double z1,
-                       const double x2,
-                       const double y2,
-                       const double z2)
+double GetX(const math::Quaternion& location)
 {
-    return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2);
+    return std::get<0>(location.GetVectorPart());
 }
 
-bool VerticesDontCollide(const Vertices& vertices)
+double GetY(const math::Quaternion& location)
 {
-    const auto& [x, y, z] = vertices;
-    const auto number_of_vertices = std::get<0>(vertices).size();
-    for (std::size_t j{}; j < number_of_vertices; j++)
+    return std::get<1>(location.GetVectorPart());
+}
+
+double GetZ(const math::Quaternion& location)
+{
+    return std::get<2>(location.GetVectorPart());
+}
+
+double SquaredDistance(const math::Quaternion q1, const math::Quaternion q2)
+{
+    const auto diff = q1 - q2;
+    return GetX(diff) * GetX(diff) +  //
+           GetY(diff) * GetY(diff) +  //
+           GetZ(diff) * GetZ(diff);
+}
+
+bool BlocksDontCollide(const Cube& cube)
+{
+    for (std::size_t j{}; j < cube.size(); j++)
     {
-        for (std::size_t k{}; k < number_of_vertices; k++)
+        for (std::size_t k{}; k < cube.size(); k++)
         {
-            if ((j != k) && (SquaredDistance(x.at(j), y.at(j), z.at(j), x.at(k), y.at(k), z.at(k)) < 0.9))
+            if ((j != k) && (SquaredDistance(cube.at(j).global_location, cube.at(k).global_location) < 0.9))
             {
                 return false;
             }
@@ -167,10 +211,10 @@ bool VerticesDontCollide(const Vertices& vertices)
 
 void PerformQuarterRotations(Cube& cube, const std::size_t index)
 {
-    if (index >= (cube.size() - 1UL))
+    const bool is_last_block{index >= (cube.size() - 1UL)};
+    if (is_last_block)
     {
-        const auto vertices = GenerateVertices(cube);
-        if (VerticesFitIn3by3Box(vertices) && VerticesDontCollide(vertices))
+        // if (BlocksFitIn3by3Box(cube) && BlocksDontCollide(cube))
         {
             Plot(cube);
         }
@@ -182,10 +226,17 @@ void PerformQuarterRotations(Cube& cube, const std::size_t index)
         std::cout << "Done here ---> |" << std::endl;
     }
 
+    const auto single_block = math::Quaternion{1.0, 0.0, 0.0};
     for (std::size_t quarter_rotations{0}; quarter_rotations < 4; quarter_rotations++)
     {
         PerformQuarterRotations(cube, index + 1);
-        cube.at(index).orientation = kQuarterRoll.AppendAsLocalRotationAfter(cube.at(index).orientation);
+
+        cube.at(index).local_orientation = kQuarterRoll.AppendAsLocalRotationAfter(cube.at(index).local_orientation);
+        cube.at(index).global_orientation =
+            cube.at(index).local_orientation.AppendAsLocalRotationAfter(cube.at(index - 1UL).global_orientation);
+        cube.at(index).global_location =
+            cube.at(index - 1UL).global_location + single_block.RotateBy(cube.at(index).global_orientation);
+
         if (index == 2UL)
         {
             std::cout << "." << std::flush;
@@ -205,6 +256,7 @@ int main()
     std::cout << "Attempting to calculate up to " << kTotalPossibleRotations << " solutions.\n";
 
     auto cube = CreateFlatCube();
+    Plot(cube);
 
     const auto t0 = std::chrono::steady_clock::now();
     PerformQuarterRotations(cube, 1);
